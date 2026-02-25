51e6cb425ffbbec035ebb84ca0f12141a5fabe63
kannan-14-08-new <kannan14082003im@gmail.com>
2026-02-25 19:42:13 +0530

    remove unwanted notification button on there

--- src/layout/TopHeader.tsx
+++ src/layout/TopHeader.tsx
@@ -215,7 +215,7 @@ export default function TopHeader({
                                 </div>
                             )}
                         </div>
-                        <div className="p-2 border-t border-gray-100 bg-gray-50/50">
+                        {/* <div className="p-2 border-t border-gray-100 bg-gray-50/50">
                             <Button 
                                 variant="ghost" 
                                 className="w-full text-[10px] font-bold uppercase tracking-widest text-gray-500 hover:text-primary rounded-xl h-9"
@@ -223,7 +223,7 @@ export default function TopHeader({
                             >
                                 View all
                             </Button>
-                        </div>
+                        </div> */}
                     </div>
                 </Dropdown>
 
